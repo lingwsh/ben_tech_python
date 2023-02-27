5d14f3b52e9aa1171be2767704a3b57e9b8147be
lingwsh <ling_wsh@foxmail.com>
2023-02-27 23:28:39 +0100

    Add numbers example code

--- python_basic/01_numbers.py
+++ python_basic/01_numbers.py
@@ -9,3 +9,12 @@ print(2 * 3)  # Output: 6
 
 # Division
 print(6 / 2)  # Output: 3.0
+
+a = 6 // 2
+print(a)  # Output: 3
+print(type(a))  # Output: <class 'int'>
+
+b = 6 / 2
+print(b)  # Output: 3.0
+print(type(b))  # Output: <class 'float'>
+
